6c5af4c14260a994d77ba415c8635dcfab6b5e57
Lea Beaulieu <beaulieu.lea@free.fr>
2021-12-08 13:30:38 +0100

    started gift edition feature

--- routes/gift.js
+++ routes/gift.js
@@ -10,23 +10,23 @@ const Gift = require("../models/Gift.model.js");
 // ##     ## ########  ########  
 
 //affichage formulaire add gift
-router.get('/add', (req,res) => {
+router.get('/add', (req, res) => {
     res.render('gift/new')
 })
 
 // traitement formulaire add gift
-router.post('/addgift', (req, res) =>{
-    const {name, category, brand, description} = req.body;
+router.post('/addgift', (req, res) => {
+    const { name, category, brand, description } = req.body;
 
-    Gift.create({name, category, brand, description})
-    .then(createdgift =>{
-        console.log(`createdgift: ${createdgift}` )
-        res.redirect('/monprofil');
-    })
-    .catch(error => {
-        console.log(`error while adding a gift: ${error}`)
-        res.render('gift/new',  { errorMessage: 'Error while adding a gift.' })
-    })
+    Gift.create({ name, category, brand, description })
+        .then(createdgift => {
+            console.log(`createdgift: ${createdgift}`)
+            res.redirect('/monprofil');
+        })
+        .catch(error => {
+            console.log(`error while adding a gift: ${error}`)
+            res.render('gift/new', { errorMessage: 'Error while adding a gift.' })
+        })
 })
 
 
@@ -38,17 +38,61 @@ router.post('/addgift', (req, res) =>{
 // ##     ## ##          ##    ##     ##  ##  ##       ##    ## 
 // ########  ########    ##    ##     ## #### ########  ######  
 
-router.get('/mygifts/:id', (req,res)=> {
+router.get('/mygifts/:id', (req, res) => {
     Gift.findById(req.params.id)
-      .then((giftDetails)=>{
-          res.render('gift/description', {giftDetails})
-      })
-      .catch(error => {
-        console.log(`error on gift details: ${error}`)
-        res.render('user/monprofil',  { errorMessage: 'Error on gift details.' })
-    })
+        .then((giftDetails) => {
+            res.render('gift/description', { giftDetails })
+        })
+        .catch(error => {
+            console.log(`error on gift details: ${error}`)
+            res.render('user/monprofil', { errorMessage: 'Error on gift details.' })
+        })
 })
 
+
+/*
+
+ ####### ######  ### ####### 
+ #       #     #  #     #    
+ #       #     #  #     #    
+ #####   #     #  #     #    
+ #       #     #  #     #    
+ #       #     #  #     #    
+ ####### ######  ###    #    
+                             
+ */
+
+
+// Route GET for listing the details of an exisiting gift
+router.get('/mygifts/:id/edit', (req, res) => {
+    Gift.findById(req.params.id)
+        .then((giftToEdit) => {
+            res.render('gift/edit', { giftToEdit })
+        })
+        .catch(error => {
+            console.log(`Error during the update of the current gift details: ${error}`)
+            res.render('gift/edit', { errorMessage: 'Error while updating the gift details. Please contact the admin team.' })
+        })
+})
+
+
+// Route POST for editing the details of an existing gift listing
+
+router.post('/mygifts/:id/edit', (req, res) => {
+    const { name, category, brand, description } = req.body;
+
+    Gift.findByIdAndUpdate(req.params.id, { name, category, brand, description }, { new: true })
+        .then(editedgift => {
+            console.log(`The edition of the gift ${editedgift.name} is properly done`)
+            res.render('gift/description');
+        })
+        .catch(error => {
+            console.log(`error while editing the following gift: ${error}`)
+            res.render('gift/edit', { errorMessage: 'Error while adding a gift.' })
+        })
+})
+
+/*
 // ########  ######## ##       ######## ######## ######## 
 // ##     ## ##       ##       ##          ##    ##       
 // ##     ## ##       ##       ##          ##    ##       
@@ -65,6 +109,6 @@ router.post('/mygifts/:id/delete', (req, res)=> {
         res.render('user/monprofil',  { errorMessage: 'Error while deleting a gift.' })
     })
 })
-
+*/
 
 module.exports = router;
