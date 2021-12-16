e9e68c184b3ec2bc69b57afe81f3a9377f5bb351
Lea Beaulieu <beaulieu.lea@free.fr>
2021-12-16 15:20:49 +0100

    started the transaction status update

--- routes/transaction.js
+++ routes/transaction.js
@@ -38,30 +38,48 @@ router.post('/gifts/:id', (req, res) => {
 */
 
 router.get('/transactionsstatus', (req, res) => {
-    Gift.findOne({user: req.session.user._id})
-    .then(mygifts => {
-        const mygiftsIds = [mygifts._id];
-        console.log('mygifts: ', mygifts)
-        console.log('mygftsIds: ', mygiftsIds)
-        Transaction.find({giftB:{ $in: mygiftsIds}})
-        .populate('giftA')
-        .populate('giftB')
-        .then(transactionsIaskedfor=> {
-            console.log('transactionIaskedfor: ', transactionsIaskedfor)
-        Transaction.find({giftA:{ $in: mygiftsIds}})
-        .populate('giftA')
-        .populate('giftB')
-        .then(transactionsIamaskedfor=> {
-            console.log('transactionIamaskedfor: ', transactionsIamaskedfor)
-            res.render('user/status', {mygifts, transactionsIaskedfor, transactionsIamaskedfor, userInSession: req.session.user }) 
-        }) 
-      })
-    })
-    .catch(error => console.log(error))  
+    Gift.findOne({ user: req.session.user._id })
+        .then(mygifts => {
+            const mygiftsIds = [mygifts._id];
+            console.log('mygifts: ', mygifts)
+            console.log('mygftsIds: ', mygiftsIds)
+            Transaction.find({ giftB: { $in: mygiftsIds } })
+                .populate('giftA')
+                .populate('giftB')
+                .then(transactionsIaskedfor => {
+                    console.log('transactionIaskedfor: ', transactionsIaskedfor)
+                    Transaction.find({ giftA: { $in: mygiftsIds } })
+                        .populate('giftA')
+                        .populate('giftB')
+                        .then(transactionsIamaskedfor => {
+                            console.log('transactionIamaskedfor: ', transactionsIamaskedfor)
+                            res.render('user/status', { mygifts, transactionsIaskedfor, transactionsIamaskedfor, userInSession: req.session.user })
+                        })
+                })
+        })
+        .catch(error => console.log(error))
 })
 
-// router.post('/transactionsstatus', (req, res) => {
 
-// })
+
+/*
+router.post('/transactionsstatus', (req, res) => {
+
+
+    const { statut } = req.body;
+
+    Transaction.findByIdAndUpdate(req.params.id, { statut })
+        .then(transactionToEdit => {
+            console.log(`The edition of the transaction status ${transactionToEdit.id} is properly done`)
+            res.redirect('/profile');
+        })
+        .catch(error => {
+            console.log(`error while editing the transaction status ${error}`)
+            res.redirect('/transactionsstatus')
+        })
+
+
+})
+*/
 
 module.exports = router;
