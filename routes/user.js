469e2198f82fe3584f8c7700a01405aa1935924a
Cindy Confiant <cindy.confiant@gmail.com>
2021-12-23 18:58:50 +0100

    letting user knows he has request to answer on profile page

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Gift = require("../models/Gift.model.js");
+const Transaction = require("../models/Transaction.model.js");
 
 // ########  ########   #######  ######## #### ##       
 // ##     ## ##     ## ##     ## ##        ##  ##       
@@ -16,14 +17,26 @@ router.get('/profile', (req, res, next) => {
   // READ user's gifts on profile page
   Gift.find({user: req.session.user._id, available : true})
     .then(giftFromDb => {
-      res.render('user/myprofile', {
-        userInSession: req.session.user,
-        giftFromDb: giftFromDb})
-    })
-    .catch (error => {
-      console.log(`error while listing gifts from Db:' ${error}`);
-      next(error)
-    })
+      mygiftsIds = [];
+        for (let i =0; i<giftFromDb.length; i++) {
+          mygiftsIds.push(giftFromDb[i].id);
+        }
+        // letting user knows the number of barters waiting for a response
+        Transaction.find({giftA:{ $in: mygiftsIds}, status:{ $ne: 'decline'}})
+          .populate('giftA')
+          .then(transactionsIamaskedfor => {
+            console.log('transactionIamaskedfor: ', transactionsIamaskedfor);
+            res.render('user/myprofile', {
+              userInSession: req.session.user,
+              giftFromDb: giftFromDb,
+              transactionsIamaskedfor
+            })
+          }).catch(error => next(error))
+      })
+      .catch (error =>{
+        console.log(`error while listing gift from Db:' ${error}`);
+        next(error);
+      }) 
 })
 
 module.exports = router;
